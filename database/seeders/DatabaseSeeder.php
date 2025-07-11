a4206e19b40edaf23a92857dcc0239d1ab0d5687
gregorio <gregorio@lepisi.ac.id>
2025-07-11 16:59:06 +0700

    created db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Application;
+use App\Models\JobPosting;
+use App\Models\Role;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $clientRole = Role::factory()->create(["name" => "client"]);
+        $freelancerRole = Role::factory()->create(["name" => "freelancer"]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $clientUser = User::factory()->for($clientRole)->create([
+            "email" => "client@example.org",
+            "password" => "password"
+        ]);
+
+        $freelancerUser = User::factory()->for($freelancerRole)->create([
+            "email" => "freelancer@example.org",
+            "password" => "password"
+        ]);
+
+        $jobPosting = JobPosting::factory()->for($clientUser, "client")->create();
+
+        $cv = UploadedFile::fake()->create("cv.pdf", 1024, "application/pdf");
+
+        $cvPath = Storage::disk("application")->putFile("cv", $cv);
+
+        $application = Application::factory()->for($jobPosting)->for($freelancerUser, "freelancer")->create([
+            "cv_path" => $cvPath
         ]);
     }
 }
